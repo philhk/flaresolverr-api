669cbc7f95468c7a92f8f1af35d0e56478bf58df
philhk <letsphil12@gmail.com>
2022-06-26 19:31:29 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -9,11 +9,6 @@ const http_proxy_agent_1 = __importDefault(require("http-proxy-agent"));
 const https_proxy_agent_1 = __importDefault(require("https-proxy-agent"));
 const axios_1 = __importDefault(require("axios"));
 class FlareSolverr {
-    options;
-    axios;
-    url;
-    ready;
-    static flareSolverrProcess;
     constructor(options) {
         this.options = options;
         if (options.hostLocally) {
@@ -35,12 +30,12 @@ class FlareSolverr {
         else
             throw new Error(`You have not specified a server or set hosting to local.`);
         this.axios = axios_1.default.create({
-            proxy: options?.proxy ? false : undefined,
-            httpAgent: options?.proxy && new ((0, http_proxy_agent_1.default)({
+            proxy: (options === null || options === void 0 ? void 0 : options.proxy) ? false : undefined,
+            httpAgent: (options === null || options === void 0 ? void 0 : options.proxy) && new ((0, http_proxy_agent_1.default)({
                 host: options.proxy.host,
                 port: options.proxy.port
             })),
-            httpsAgent: options?.proxy && new ((0, https_proxy_agent_1.default)({
+            httpsAgent: (options === null || options === void 0 ? void 0 : options.proxy) && new ((0, https_proxy_agent_1.default)({
                 host: options.proxy.host,
                 port: options.proxy.port
             })),
@@ -50,10 +45,7 @@ class FlareSolverr {
     async createSession(request) {
         const CMD = 'sessions.create';
         const request_ = {
-            data: {
-                ...request?.data,
-                cmd: CMD
-            }
+            data: Object.assign(Object.assign({}, request === null || request === void 0 ? void 0 : request.data), { cmd: CMD })
         };
         try {
             const response = await this.axios.post(this.url, request_.data);
@@ -83,10 +75,7 @@ class FlareSolverr {
     async destroySession(request) {
         const CMD = 'sessions.destroy';
         const request_ = {
-            data: {
-                ...request.data,
-                cmd: CMD
-            }
+            data: Object.assign(Object.assign({}, request.data), { cmd: CMD })
         };
         try {
             const response = await this.axios.post(this.url, request_.data);
@@ -100,11 +89,7 @@ class FlareSolverr {
     async getRequest(request) {
         const CMD = 'request.get';
         const request_ = {
-            data: {
-                ...request.data,
-                cmd: CMD,
-                maxTimeout: this.options.maxTimeout,
-            }
+            data: Object.assign(Object.assign({}, request.data), { cmd: CMD, maxTimeout: this.options.maxTimeout })
         };
         try {
             const response = await this.axios.post(this.url, request_.data);
@@ -125,28 +110,19 @@ class FlareSolverr {
                 console.log(stderr);
         });
         return new Promise((resolve, reject) => {
+            var _a;
             setTimeout(() => {
-                listener?.destroy();
+                listener === null || listener === void 0 ? void 0 : listener.destroy();
                 reject('FlareSolverr proxy failed to start.');
             }, 10000);
-            const listener = this.flareSolverrProcess.stdout?.on('data', (data) => {
+            const listener = (_a = this.flareSolverrProcess.stdout) === null || _a === void 0 ? void 0 : _a.on('data', (data) => {
                 if (!data.includes('Listening on http://0.0.0.0:8191'))
                     return;
-                listener?.destroy();
+                listener === null || listener === void 0 ? void 0 : listener.destroy();
                 resolve(true);
             });
         });
     }
 }
 exports.FlareSolverr = FlareSolverr;
-(async () => {
-    const flareSolverr = await new FlareSolverr({
-        maxTimeout: 60000,
-        hostLocally: true,
-        flaresolverrPath: './flaresolverr/flaresolverr.exe'
-    }).ready;
-    await flareSolverr.createSession();
-    const data = await flareSolverr.getRequest({ data: { url: 'https://disboard.org' } });
-    console.log(data);
-})();
 //# sourceMappingURL=index.js.map
